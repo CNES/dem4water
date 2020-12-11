9394852279f3ad09974b05c4be555bfb216e6153
Aurelien BRICIER <aurelien.bricier@c-s.fr>
2020-12-11 14:16:58 +0000

    FEAT: init plot with S(Zi)

--- szi_to_model.py
+++ szi_to_model.py
@@ -19,21 +19,50 @@ The output
 
 import os
 import sys
+import logging
 import argparse
-
+import numpy as np
+import matplotlib.pyplot as plt
+from statistics import median
 
 def main(arguments):
 
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('infile', help="Input file", type=argparse.FileType('r'))
-    parser.add_argument('-o', '--outfile', help="Output file",
-                        default=sys.stdout, type=argparse.FileType('w'))
+    parser.add_argument('-i',
+                        '--infile',
+                        help="Input file")
+    parser.add_argument('-o',
+                        '--outfile',
+                        help="Output file")
+    parser.add_argument('--loglevel',
+                        help="Log Level",
+                        default="INFO")
 
     args = parser.parse_args(arguments)
 
     print(args)
+
+    numeric_level = getattr(logging, args.loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % args.loglevel)
+    logging_format = '%(asctime)s - %(filename)s:%(lineno)s - %(levelname)s - %(message)s'
+    logging.basicConfig(stream=sys.stdout, level=numeric_level, format=logging_format)
+    logging.info("Starting szi_to_model.py")
+
+    data = np.loadtxt(args.infile)
+    x = data[:, 0]
+    y = data[:, 1]
+
+    print("x: ")
+    print(x[:-1])
+
+    logging.info("med(zi):"+ str(median(x[:-1])) +" - med(Szi): "+ str(median(y[:-1])))
+
+    plt.plot(x, y, 'ro')
+    plt.savefig(args.outfile)
+
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
