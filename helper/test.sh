cfff9525ac9d430fbee9afb0718a476fe288700e
Aurelien BRICIER <aurelien.bricier@c-s.fr>
2020-12-18 10:40:17 +0000

    FEAT: Better global helper (radius as a parameter)

--- helper/test.sh
+++ helper/test.sh
@@ -7,15 +7,22 @@
 # :created: 2020
 # ----
 
-cd ~/dem4water/dem4water
+SRC_DIR="/home/ad/briciera/dem4water/dem4water"
 ROOT_DIR="/home/ad/briciera/scratch/HSV"
 DAM=${1:-"Agly"}
+RADIUS=${2:-7500}
 
-[ -d "$ROOT_DIR/$DAM/tmp" ] || mkdir -p "$ROOT_DIR/$DAM/tmp"
 
-export OTB_MAX_RAM_HINT=4000
-export OTB_LOGGER_LEVEL=CRITICAL
 module load otb/7.2-python3.7.2
+[ -z "$OTB_MAX_RAM_HINT" ] && export OTB_MAX_RAM_HINT=4000
+# [ -z "$OTB_LOGGER_LEVEL" ] && export OTB_LOGGER_LEVEL=CRITICAL
+export OTB_LOGGER_LEVEL=CRITICAL
+echo "OTB_MAX_RAM_HINT: $OTB_MAX_RAM_HINT"
+echo "OTB_LOGGER_LEVEL: $OTB_LOGGER_LEVEL"
+
+
+cd $SRC_DIR
+[ -d "$ROOT_DIR/${DAM}_${RADIUS}/tmp" ] || mkdir -p "$ROOT_DIR/${DAM}_${RADIUS}/tmp"
 
 
 #Lat: 42.7461
@@ -30,8 +37,8 @@ module load otb/7.2-python3.7.2
 # Bottom Alt: 123.55
 
 
-if [ -f "$ROOT_DIR/$DAM/dem_extract-$DAM.tif" ] \
-  && [ -f "$ROOT_DIR/$DAM/wmap_extract-$DAM.tif" ] ; then
+if [ -f "$ROOT_DIR/${DAM}_${RADIUS}/dem_extract-$DAM.tif" ] \
+  && [ -f "$ROOT_DIR/${DAM}_${RADIUS}/wmap_extract-$DAM.tif" ] ; then
   echo "Extracts already available --> Skipping area_mapping."
 else
   python3 area_mapping.py \
@@ -39,30 +46,40 @@ else
     --watermap "../data/T31TDH/all_cumul.tif" \
     --dem      "../data/dem/dem.vrt" \
     --name     "$DAM" \
-    --radius   2000 \
-    --out      "$ROOT_DIR/$DAM" \
-    --loglevel "DEBUG"
+    --radius   5000 \
+    --out      "$ROOT_DIR/${DAM}_${RADIUS}" \
+    # --debug
+
 fi
 
+if [ -f "$ROOT_DIR/${DAM}_${RADIUS}/contour.shp" ] ; then
+  echo "Contours already available --> Skipping gdal_contour."
+else
+  gdal_contour -p -i 10.0 \
+    "$ROOT_DIR/${DAM}_${RADIUS}/dem_extract-Agly.tif" \
+    "$ROOT_DIR/${DAM}_${RADIUS}/contour.shp"
+fi
 
-if [ -f "$ROOT_DIR/$DAM/szi_wmap.dat" ] ; then
+if [ -f "$ROOT_DIR/${DAM}_${RADIUS}/szi_wmap.dat" ] ; then
   echo "S(Zi)s already available --> Skipping szi_from_watermap."
 else
   python3 szi_from_watermap.py \
-    --watermap "$ROOT_DIR/$DAM/wmap_extract-$DAM.tif" \
-    --dem      "$ROOT_DIR/$DAM/dem_extract-$DAM.tif" \
-    --tmp      "$ROOT_DIR/$DAM/tmp" \
+    --watermap "$ROOT_DIR/${DAM}_${RADIUS}/wmap_extract-$DAM.tif" \
+    --dem      "$ROOT_DIR/${DAM}_${RADIUS}/dem_extract-$DAM.tif" \
+    --tmp      "$ROOT_DIR/${DAM}_${RADIUS}/tmp" \
     --zmin     123.55 \
     --zmax     184.11 \
     --step     2 \
-    --outfile  "$ROOT_DIR/$DAM/szi_wmap.dat" \
-    --loglevel "DEBUG"
+    --outfile  "$ROOT_DIR/${DAM}_${RADIUS}/szi_wmap.dat" \
+    --debug
+
 fi
 
 
-if [ -f "$ROOT_DIR/$DAM/szi_wmap.dat" ] ; then
+if [ -f "$ROOT_DIR/${DAM}_${RADIUS}/szi_wmap.dat" ] ; then
   python3 szi_to_model.py \
-    --infile   "$ROOT_DIR/$DAM/szi_wmap.dat" \
-    --outfile  "$ROOT_DIR/$DAM/szi_wmap.png" \
-    --loglevel "INFO"
+    --infile   "$ROOT_DIR/${DAM}_${RADIUS}/szi_wmap.dat" \
+    --outfile  "$ROOT_DIR/${DAM}_${RADIUS}/szi_wmap.png" \
+    --debug
+
 fi
