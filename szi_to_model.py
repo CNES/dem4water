31da8854ea951b8c6e9771493752d35aae93a9b5
Aurelien BRICIER <aurelien.bricier@c-s.fr>
2020-12-18 10:38:51 +0000

    FEAT: Better debug/logger configuration

--- szi_to_model.py
+++ szi_to_model.py
@@ -36,19 +36,19 @@ def main(arguments):
     parser.add_argument('-o',
                         '--outfile',
                         help="Output file")
-    parser.add_argument('--loglevel',
-                        help="Log Level",
-                        default="INFO")
+    parser.add_argument('--debug',
+                        action='store_true',
+                        help='Activate Debug Mode')
 
     args = parser.parse_args(arguments)
 
     print(args)
 
-    numeric_level = getattr(logging, args.loglevel.upper(), None)
-    if not isinstance(numeric_level, int):
-        raise ValueError('Invalid log level: %s' % args.loglevel)
     logging_format = '%(asctime)s - %(filename)s:%(lineno)s - %(levelname)s - %(message)s'
-    logging.basicConfig(stream=sys.stdout, level=numeric_level, format=logging_format)
+    if (args.debug is True):
+        logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format=logging_format)
+    else:
+        logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=logging_format)
     logging.info("Starting szi_to_model.py")
 
     data = np.loadtxt(args.infile)
