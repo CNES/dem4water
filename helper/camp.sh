a4e4260476510acdd288eda35a1c71be0f83e74c
Aurelien BRICIER <aurelien.bricier@c-s.fr>
2021-01-27 14:09:19 +0000

    FEAT: Updated camp script (log, filenames...)

--- helper/camp.sh
+++ helper/camp.sh
@@ -8,7 +8,10 @@
 # ----
 
 SRC_DIR="/home/ad/briciera/dem4water/dem4water"
-ROOT_DIR="/home/ad/briciera/scratch/HSV/camp_20210122"
+ROOT_DIR="/home/ad/briciera/scratch/HSV/camp_20210126"
+DB_PATH="../data/DB_Barrages_Fixed_v2/DB_Barrages_Fixed.shp"
+DEM_PATH="../data/dem/dem.vrt"
+WMAP_PATH="../data/wmap/wmap_30.vrt"
 RADIUS=${1:-5000}
 
 module load otb/7.2-python3.7.2
@@ -20,62 +23,57 @@ echo "OTB_LOGGER_LEVEL: $OTB_LOGGER_LEVEL"
 
 cd $SRC_DIR
 
-# declare -a StringArray=("Agly" "Cavayère" "Montbel" "Vinca" "Puyvalador"
-                        # "Matemale" "Bouillouses" "Naguilhes" "Ganguise"
-                        # "Aussoue" "Gimone" "Marcaoue" "Pessoulens"
-                        # "Comberouger" "Malause" "Tordre" "Fontbouysse"
-                        # "Pinet" "Bally" "Araing")
-
-declare -a StringArray=('Agly'    'Astarac'  'Aussoue'             'Grande Patures'
-                        'Balerme' 'Cammazes' 'Filhet'              'Pla de Soulcem'
-                        'Galaube' 'Ganguise' 'Izourt'              'Raschas'
-                        'Laparan' 'Laprade'  'Matemale'            'Gouyre'
-                        'Salagou' 'Montbel'  'Monts d'\''Orb (Avène)' 'Olivettes')
-                        # "Cap de Long" "Pareloup" "Portillon" "Puydarrieux"
-                        # "Puylaurent" "Saint Ferréol" "Saint géraud" "Sainte Peyres"
-                        # "Tordre" "Vinca" "Boues - Serres-Rustaing" "Miélan"
-                        # "Charpal" "Villeneuve de la raho" "Gouyre" "Lac d'Oô"
-                        # "Puyvalador" "Arrêt-Darré" "Louet" "Gimone"
+declare -a StringArray=('Agly'        'Astarac'       'Aussoue'      'Grande Patures'
+                        'Balerme'     'Cammazes'      'Filhet'       'Pla de Soulcem'
+                        'Galaube'     'Ganguise'      'Izourt'       'Raschas'
+                        'Laparan'     'Laprade'       'Matemale'     'Gouyre'
+                        'Salagou'     'Montbel'       'Olivettes'    'Monts d'\''Orb (Avène)'
+                        'Cap de Long' 'Pareloup'      'Tordre'       'Vinca'
+                        'Puylaurent'  'Saint Ferréol' 'Saint géraud' 'Sainte Peyres'
+                        'Charpal'     'Gouyre'        'Puyvalador'   'Villeneuve de la raho')
 
 for DAMNAME in "${StringArray[@]}"; do
 
   DAM=${DAMNAME// /_}
 
   [ -d "$ROOT_DIR/${DAM}_${RADIUS}/tmp" ] || mkdir -p "$ROOT_DIR/${DAM}_${RADIUS}/tmp"
+  [ -d "$ROOT_DIR/${DAM}_${RADIUS}/log" ] || mkdir -p "$ROOT_DIR/${DAM}_${RADIUS}/log"
 
   if [ -f "$ROOT_DIR/${DAM}_${RADIUS}/dem_extract-$DAM.tif" ] \
     && [ -f "$ROOT_DIR/${DAM}_${RADIUS}/wmap_extract-$DAM.tif" ] ; then
     echo "Extracts already available --> Skipping area_mapping."
   else
-    python3 area_mapping.py \
+    # python3 area_mapping.py \
+    python3 area_mapping.py --debug \
       --name     "${DAMNAME}" \
-      --infile   "../data/synth_names.shp" \
-      --watermap "../data/wmap/wmap.vrt" \
-      --dem      "../data/dem/dem.vrt" \
+      --infile   "${DB_PATH}" \
+      --watermap "${WMAP_PATH}" \
+      --dem      "${DEM_PATH}" \
       --radius   "$RADIUS" \
-      --out      "$ROOT_DIR/${DAM}_${RADIUS}" 2>&1 | tee "$ROOT_DIR/${DAM}_${RADIUS}/area_mapping.log"
-      # --out      "$ROOT_DIR/${DAM}_${RADIUS}" --debug 2>&1 | tee "$ROOT_DIR/${DAM}_${RADIUS}/area_mapping.log"
-
+      --out      "$ROOT_DIR/${DAM}_${RADIUS}" \
+      2>&1 | tee "$ROOT_DIR/${DAM}_${RADIUS}/log/area_mapping.log"
   fi
 
-  python3 szi_from_contourline.py \
+  # python3 szi_from_contourline.py \
+  python3 szi_from_contourline.py --debug \
     --name         "${DAMNAME}" \
-    --infile       "../data/synth_names.shp" \
+    --infile       "${DB_PATH}" \
     --watermap     "$ROOT_DIR/${DAM}_${RADIUS}/wmap_extract-$DAM.tif" \
     --dem          "$ROOT_DIR/${DAM}_${RADIUS}/dem_extract-$DAM.tif"  \
     --radius       "$RADIUS" \
     --pdbstep      5 \
     --pdbradius    500 \
-    --elevsampling 1 \
+    --elevsampling 5 \
     --elevoffset   60 \
     --tmp          "$ROOT_DIR/${DAM}_${RADIUS}/tmp" \
-    --out          "$ROOT_DIR/${DAM}_${RADIUS}" --debug 2>&1 | tee "$ROOT_DIR/${DAM}_${RADIUS}/szi_from_contourline.log"
-    # --out          "$ROOT_DIR/${DAM}_${RADIUS}"  2>&1 | tee "$ROOT_DIR/${DAM}_${RADIUS}/szi_from_contourline.log"
+    --out          "$ROOT_DIR/${DAM}_${RADIUS}" \
+    2>&1 | tee "$ROOT_DIR/${DAM}_${RADIUS}/log/szi_from_contourline.log"
 
   python3 cutline_score.py \
     --infile   "$ROOT_DIR/${DAM}_${RADIUS}/${DAM}_cutline.json" \
     --watermap "$ROOT_DIR/${DAM}_${RADIUS}/wmap_extract-$DAM.tif" \
     --out      "$ROOT_DIR/${DAM}_${RADIUS}/tmp" \
-    --debug 2>&1 | tee "$ROOT_DIR/${DAM}_${RADIUS}/cutline_score.log"
+    --debug    \
+    2>&1 | tee "$ROOT_DIR/${DAM}_${RADIUS}/log/cutline_score.log"
 
-  done
+done
