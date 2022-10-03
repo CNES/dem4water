fd6bf0e2cd91e189b22fb2c87e0dcb7a9a8c2b28
Benjamin TARDY <benjamin.tardy@csgroup.eu>
2022-10-03 12:40:17 +0000

    FEAT: compute elev diff between DAM elev and PDB

--- report_campaign.py
+++ report_campaign.py
@@ -5,10 +5,24 @@ import glob
 import pandas as pd
 import numpy as np
 import json
+
+
+def check_cutline_score(name, infile ):
+    # print(infile)
+    with open(infile, "r",encoding="utf-8") as inf:
+        text = inf.read().split("\n")
+        for line in text:
+            # input(line)
+            if "Score=" in line:
+                value = float(line.split("Score=")[-1])
+                print(name, ": ", value)
+                return name, value
+    return name, 0
+                
 def ensure_combo(folder):
     content = glob.glob(os.path.join(folder,"*combo.png"))
     if len(content) != 1:
-        print(f"erreur barrage {folder}")
+        print(f"Combo graph not found for {folder}")
 
 
 def check_pbd_delta(folder, name):
@@ -47,20 +61,27 @@ def main(arguments):
 
     args = parser.parse_args(arguments)
 
-    list_dir = glob.glob(args.infolder+"/*")
+
+    logs_dir = os.path.join(args.infolder, "logs_pbs")
+    
+    list_dir = glob.glob(args.infolder+"/camp/*")
     names = []
     deltas = []
     list_dir.sort()
+    cutline_score = {}
     for folder in list_dir :
         dam_name = folder.split("/")[-1]
         ensure_combo(folder)
         ret =  check_pbd_delta(folder, dam_name)
         deltas.append(ret)
-    
+        _, val = check_cutline_score(dam_name, os.path.join(logs_dir, f"{dam_name}_out.log"))
+        cutline_score[dam_name] = val
     deltas = sorted(deltas, key=lambda x: x[1])
     # print(deltas)
     for k, v, v2 in deltas:
         print(f"|{k}|{v}|{v2}|")
-        
+    print("\n"*2)
+    for k,v in cutline_score.items():
+        print(f"|{k}|{v}|")
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
