6dbf31feb380314f9d2e7197d14890436b304699
Benjamin TARDY <benjamin.tardy@csgroup.eu>
2022-12-05 13:01:35 +0000

    ADD: function to produce the water body mask

---
+++ src/water_body.py
@@ -0,0 +1,80 @@
+import os
+import otbApplication as otb
+import geopandas as gpd
+import numpy as np
+import logging
+
+
+def create_water_mask(watermap, water_thres=0.15):
+    """Find the water body"""
+
+    binary_wmap = watermap.replace(".tif","_binary.tif")
+    bandmath = otb.Registry.CreateApplication("BandMath")
+    bandmath.SetParameterStringList("il", [watermap])
+    bandmath.SetParameterString("exp", f"im1b1>{water_thres}?1:0")
+    bandmath.SetParameterString("out", binary_wmap)
+    bandmath.ExecuteAndWriteOutput()
+    shp_wmap = watermap.replace(".tif", ".shp")
+    
+    cmd = f"gdal_polygonize.py {binary_wmap} {shp_wmap}"
+    os.system(cmd)
+
+    if not os.path.exists(shp_wmap):
+        raise OSError(f"{shp_wmap} not exists")
+    return shp_wmap
+
+
+def get_largest_water_body(shp_map, watermap):
+    gdf = gpd.GeoDataFrame().from_file(shp_wmap)
+    gdf["area"] = gdf.geometry.area
+    # Get only water bodies
+    gdf_filtered = gdf[gdf.DN == 1] 
+    gdf_out  = gdf_filtered[gdf_filtered.area == max(gdf_filtered.area)]
+    water_body = watermap.replace(".tif", "_water_body.shp")
+    gdf_out.to_file(water_body)
+
+    # Rasterize
+    water_body_im = water_body.replace(".shp", ".tif")
+    rasterize = otb.Registry.CreateApplication("Rasterization")
+    rasterize.SetParameterString("in", water_body)
+    rasterize.SetParameterString("im", watermap)
+    rasterize.SetParameterString("out", water_body_im)
+    rasterize.SetParameterString("mode", "binary")
+    rasterize.SetParameterString("mode.binary.foreground", "1")
+    rasterize.ExecuteAndWriteOutput()
+    return water_body_im
+
+
+def compute_area_from_water_body(daminfo, shp_wmap):
+    """Compute intersection between insider and water bodies"""
+    gdf_wmap = gpd.GeoDataFrame().from_file(shp_wmap)
+    gdf_wmap["area"] = gdf_wmap.geometry.area
+    gdf_filtered = gdf_wmap[gdf_wmap.DN==1]
+
+    gdf_dam = gpd.GeoDataFrame().from_file(daminfo)
+    gdf_insider = gdf_dam[gdf_dam.name=="Insider"]
+    # convert point to polygon
+    gdf_insider.geometry = gdf_insider.geometry.buffer(1)
+    result = gdf_filtered.sjoin(gdf_insider)
+    area = np.array(result.area)
+    if len(area) != 1:
+        logging.info("There is no matching waterbody. Check your data")
+        return 0
+    return area[0]
+
+
+def compute_area_from_database_geom(database, damname,shp_wmap):
+    gdf_db = gpd.GeoDataFrame().from_file(database)
+    gdf_db = gdf_db[gdf_db.DAM_NAME==damname]
+
+    gdf_wmap = gpd.GeoDataFrame().from_file(shp_wmap)
+    gdf_wmap = gdf_wmap[gdf_wmap.DN==1]
+    gdf_db = gdf_db.to_crs(gdf_wmap.crs)
+    # result = gdf_wmap.sjoin(gdf_db)
+    result = gdf_wmap.overlay(gdf_db, how='intersection')
+    result["area"] = result.geometry.area
+    area = np.array(result.area)
+    res = 0
+    for val in area:
+        res += val
+    return res
