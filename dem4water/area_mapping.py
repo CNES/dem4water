6b9aa90bbc763b644059ca21af6147c0884b8091
Benjamin TARDY <benjamin.tardy@csgroup.eu>
2023-10-26 09:23:41 +0000

    WIP: avoid ROI if dem is downloaded

--- dem4water/area_mapping.py
+++ dem4water/area_mapping.py
@@ -64,7 +64,7 @@ def area_mapping(
     dam_name = ""
     # dam_path = ""
     dam_404 = True
-
+    downloaded_dem = False
     for feature in layer:
         if str(int(feature.GetField(str(id_db)))) == str(dam_id):
             # Compute radius
@@ -85,27 +85,31 @@ def area_mapping(
     layer.ResetReading()
     if dem is None:
         # Download DEM
-        long_radius = abs(bbox[2] - bbox[3])
-        lat_radius = abs(bbox[0] - bbox[1])
-        if long_radius > lat_radius:
-            rad = long_radius
+        if not glob.glob(os.path.join(output_download_path, "COP30*")):
+            long_radius = abs(bbox[2] - bbox[3])
+            lat_radius = abs(bbox[0] - bbox[1])
+            if long_radius > lat_radius:
+                rad = long_radius
+            else:
+                rad = lat_radius
+            params = Topography.DEFAULT.copy()
+            params = {
+                "dem_type": "COP30",
+                "south": bbox[2] - 2 * rad,
+                "north": bbox[3] + 2 * rad,
+                "west": bbox[0] - 2 * rad,
+                "east": bbox[1] + 2 * rad,
+                "output_format": "GTiff",
+                "cache_dir": output_download_path,
+            }
+            boulder = Topography(**params)
+            boulder.fetch()
+            # TODO: comment est gérée la projection
         else:
-            rad = lat_radius
-        params = Topography.DEFAULT.copy()
-        params = {
-            "dem_type": "COP30",
-            "south": bbox[2] - 2 * rad,
-            "north": bbox[3] + 2 * rad,
-            "west": bbox[0] - 2 * rad,
-            "east": bbox[1] + 2 * rad,
-            "output_format": "GTiff",
-            "cache_dir": output_download_path,
-        }
-        boulder = Topography(**params)
-        boulder.fetch()
-        # TODO: comment est gérée la projection
+            print("DEM already downloaded")
         dem = glob.glob(os.path.join(output_download_path, "COP30*"))[0]
-
+        downloaded_dem = True
+        print(f"Input downloaded DEM found : {dem}")
     if dam_404 is True:
         logging.error("404 - Dam Not Found: {dam_id} is not present in {infile}")
     calt = float(
@@ -116,56 +120,60 @@ def area_mapping(
     )
     if watermap is None:
         # Download occurence
-        DATASET_NAME = "occurrence"
-        lg = abs(bbox[0])
-        lt = abs(bbox[2])
+        if not glob.glob(os.path.join(output_download_path, "occurrence*")):
+            DATASET_NAME = "occurrence"
+            lg = abs(bbox[0])
+            lt = abs(bbox[2])
 
-        long = int(lg // 10 * 10) + 10
-        lat = int(lt // 10 * 10) + 10
+            long = int(lg // 10 * 10) + 10
+            lat = int(lt // 10 * 10) + 10
 
-        if bbox[0] < 0 and bbox[1] < 0:
-            long = str(long) + "W"
-        elif bbox[0] > 0 and bbox[1] > 0:
-            long = str(long) + "E"
-        if bbox[2] < 0 and bbox[3] < 0:
-            lat = str(lat) + "S"
-        if bbox[2] > 0 and bbox[3] > 0:
-            lat = str(lat) + "N"
+            if bbox[0] < 0 and bbox[1] < 0:
+                long = str(long) + "W"
+            elif bbox[0] > 0 and bbox[1] > 0:
+                long = str(long) + "E"
+            if bbox[2] < 0 and bbox[3] < 0:
+                lat = str(lat) + "S"
+            if bbox[2] > 0 and bbox[3] > 0:
+                lat = str(lat) + "N"
 
-        filename = DATASET_NAME + "_" + str(long) + "_" + str(lat) + "v1_4_2021.tif"
-        url = os.path.join(
-            "https://storage.googleapis.com/global-surface-water/downloads2021",
-            DATASET_NAME,
-            filename,
-        )
-        code = urllib.request.urlopen(url).getcode()
-        if code != 404:
-            print("Downloading " + url + ")")
-            urllib.request.urlretrieve(
-                url, os.path.join(output_download_path, filename)
+            filename = DATASET_NAME + "_" + str(long) + "_" + str(lat) + "v1_4_2021.tif"
+            url = os.path.join(
+                "https://storage.googleapis.com/global-surface-water/downloads2021",
+                DATASET_NAME,
+                filename,
             )
+            code = urllib.request.urlopen(url).getcode()
+            if code != 404:
+                print("Downloading " + url + ")")
+                urllib.request.urlretrieve(
+                    url, os.path.join(output_download_path, filename)
+                )
+            else:
+                print(url + " not found")
         else:
-            print(url + " not found")
-
+            print("Occurence already download")
         occurrence = glob.glob(os.path.join(output_download_path, "occurrence*"))[0]
+        print(f"Downloaded Occurence found: {occurrence}")
         watermap = occurrence.replace(".tif", "_reproject.tif")
 
         # TODO : vérifier que l'epsg doit etre forcé en 32630
         # pk pas 2154 ?
         # Aligner sur la BD ? Non doit être en coords métriques si calcul d'aire
-        dst_crs = "EPSG:32630"
-        src_ds = gdal.Open(occurrence)
+        if not os.path.exists(watermap):
+            dst_crs = "EPSG:32630"
+            src_ds = gdal.Open(occurrence)
 
-        largeur = src_ds.RasterXSize
-        hauteur = src_ds.RasterYSize
-        gdal.Warp(
-            watermap,
-            src_ds,
-            dstSRS=dst_crs,
-            width=largeur,
-            height=hauteur,
-            resampleAlg=gdal.GRA_Cubic,
-        )
+            largeur = src_ds.RasterXSize
+            hauteur = src_ds.RasterYSize
+            gdal.Warp(
+                watermap,
+                src_ds,
+                dstSRS=dst_crs,
+                width=largeur,
+                height=hauteur,
+                resampleAlg=gdal.GRA_Cubic,
+            )
 
     src = osr.SpatialReference()
 
@@ -196,33 +204,35 @@ def area_mapping(
 
     # TODO: extract dem before superimpose ?
     # Get information form DEM
-    with rio.open(dem) as dem_raster:
-        epsg_dem = dem_raster.crs.to_epsg()
-        dst = osr.SpatialReference()
-        dst.SetAxisMappingStrategy(osr.OAMS_TRADITIONAL_GIS_ORDER)
-        dst.ImportFromEPSG(epsg_dem)
-        coord_trans = osr.CoordinateTransformation(src, dst)
-        point_dem = ogr.Geometry(ogr.wkbPoint)
-        point_dem.AddPoint(clon, clat)
-        point_dem.Transform(coord_trans)
-        logging.info(
-            f"Coordinates for dem ROI: {point_dem.GetX()} - {point_dem.GetY()}"
-        )
+    if not downloaded_dem:
+        with rio.open(dem) as dem_raster:
+            epsg_dem = dem_raster.crs.to_epsg()
+            dst = osr.SpatialReference()
+            dst.SetAxisMappingStrategy(osr.OAMS_TRADITIONAL_GIS_ORDER)
+            dst.ImportFromEPSG(epsg_dem)
+            coord_trans = osr.CoordinateTransformation(src, dst)
+            point_dem = ogr.Geometry(ogr.wkbPoint)
+            point_dem.AddPoint(clon, clat)
+            point_dem.Transform(coord_trans)
+            logging.info(
+                f"Coordinates for dem ROI: {point_dem.GetX()} - {point_dem.GetY()}"
+            )
 
-        extract_roi_parameters_dem = ExtractROIParam(
-            mode="radius",
-            mode_radius_r=float(radius) * 2,
-            mode_radius_unitr="phy",
-            mode_radius_unitc="phy",
-            mode_radius_cx=point_dem.GetX(),
-            mode_radius_cy=point_dem.GetY(),
-            dtype="float",
-        )
-        ext_dem, profile_dem = extract_roi(dem_raster, extract_roi_parameters_dem)
-        roi_dem = out_dem.replace(".tif", "roi.tif")
-        save_image(ext_dem, profile_dem, roi_dem)
+            extract_roi_parameters_dem = ExtractROIParam(
+                mode="radius",
+                mode_radius_r=float(radius) * 2,
+                mode_radius_unitr="phy",
+                mode_radius_unitc="phy",
+                mode_radius_cx=point_dem.GetX(),
+                mode_radius_cy=point_dem.GetY(),
+                dtype="float",
+            )
+            ext_dem, profile_dem = extract_roi(dem_raster, extract_roi_parameters_dem)
+            roi_dem = out_dem.replace(".tif", "roi.tif")
+            save_image(ext_dem, profile_dem, roi_dem)
         # input("dem extracted")
-
+    else:
+        roi_dem = dem
     superimpose_app = SuperimposeParam(interpolator="bco", dtype="float")
     app, profile_app = superimpose(
         rio.open(roi_dem), rio.open(out_wmap), superimpose_app
